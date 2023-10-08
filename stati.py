6e5fa86b7721d39348c2293f212fdd2c1093b37c
sucks555 <102584033+sucks-555@users.noreply.github.com>
2023-10-09 07:46:43 +0900

    Update stati.py

--- stati.py
+++ stati.py
@@ -1,119 +1,137 @@
 import math
 
-def statistics(data):
-  if type(data) == str:
-    data = data.strip('[]').split(',')
-    data = [float(x) for x in data]
-  sorted_data = sorted(data)
-  length = len(sorted_data)
-  half = length // 2
-  box, Outlier = [], []
-  count, box_count, Total, Range25per, max_count = 0,0,0,0,0
-  mode_value = None
-  Total = sum(sorted_data)
-  Average = Total / length
-  for number in range(length):
-    if Average * 3 <= sorted_data[number]:
-      Outlier.append(sorted_data[number])
-  if length % 2 == 0: Range50per = (sorted_data[half - 1] + sorted_data[half]) / 2
-  else: Range50per = sorted_data[half]
-  if (half / 2).is_integer():
-    for h in range(half):
-      if h == (half // 2) - 1:count += sorted_data[h]
-      elif h == half // 2:count += sorted_data[h]
-    Range25per = count / 2
-    for j in range(length):
-      if j >= half + 1:
-        box.append(sorted_data[j])
-        box_count += 1
-    Range75per = (box[box_count // 2] + box[(box_count // 2) - 1]) / 2
-  else:
-    for h in range(half):
-      if h == half // 2:Range25per += sorted_data[h]
-    for j in range(length):
-      if j >= half:
-        box.append(sorted_data[j])
-        box_count += 1
-    Range75per = box[box_count // 2]
-  Scope = sorted_data[length - 1] - sorted_data[0]
-  Quartilerange = Range75per - Range25per
-  Min_value, Max_value = sorted_data[0], sorted_data[length - 1]
-  for d in sorted_data:
-    data_count = sorted_data.count(d)
-    if data_count > max_count:max_count,mode_value = data_count,d
-  result = {
-    'len': length,
-    'total': Total,
-    'average': Average,
-    'max': Max_value,
-    'min': Min_value,
-    'median': Range50per,
-    'IQR': Quartilerange,
-    'range': Scope,
-    'mode': mode_value,
-    'outliers': Outlier,
-    'Q1': Range25per,
-    'Q2': Range50per,
-    'Q3': Range75per,
-    'Q': {
-      'Q1': Range25per,
-      'Q2': Range50per,
-      'Q3': Range75per
+class Statistics:
+  def __init__(self, data=None):
+    self.data = data
+
+  def set_data(self, data):
+    self.data = data
+
+  def calculate_statistics(self):
+    if isinstance(self.data, str):
+      self.data = self.data.strip('[]').split(',')
+      self.data = [float(x) for x in self.data]
+    sorted_data = sorted(self.data)
+    length = len(sorted_data)
+    half = length // 2
+    box, outliers = [], []
+    count, box_count, total, range_25per, max_count = 0, 0, 0, 0, 0
+    mode_value = None
+    total = sum(sorted_data)
+    average = total / length
+    for number in range(length):
+      if average * 3 <= sorted_data[number]:
+        outliers.append(sorted_data[number])
+    if length % 2 == 0:
+      range_50per = (sorted_data[half - 1] + sorted_data[half]) / 2
+    else:
+      range_50per = sorted_data[half]
+    if (half / 2).is_integer():
+      for h in range(half):
+        if h == (half // 2) - 1:
+          count += sorted_data[h]
+        elif h == half // 2:
+          count += sorted_data[h]
+      range_25per = count / 2
+      for j in range(length):
+        if j >= half + 1:
+          box.append(sorted_data[j])
+          box_count += 1
+      range_75per = (box[box_count // 2] + box[(box_count // 2) - 1]) / 2
+    else:
+      for h in range(half):
+        if h == half // 2:
+          range_25per += sorted_data[h]
+      for j in range(length):
+        if j >= half:
+          box.append(sorted_data[j])
+          box_count += 1
+      range_75per = box[box_count // 2]
+    scope = sorted_data[length - 1] - sorted_data[0]
+    quartile_range = range_75per - range_25per
+    min_value, max_value = sorted_data[0], sorted_data[length - 1]
+    for d in sorted_data:
+      data_count = sorted_data.count(d)
+      if data_count > max_count:
+        max_count, mode_value = data_count, d
+    result = {
+      'length': length,
+      'total': total,
+      'average': average,
+      'max': max_value,
+      'min': min_value,
+      'median': range_50per,
+      'IQR': quartile_range,
+      'range': scope,
+      'mode': mode_value,
+      'outliers': outliers,
+      'Q1': range_25per,
+      'Q2': range_50per,
+      'Q3': range_75per,
+      'Q': {'Q1': range_25per, 'Q2': range_50per, 'Q3': range_75per}
     }
-  }
-  return result
+    return result
 
-def Difference(data):
-  if type(data) == str:
-    data = data.strip('[]').split(',')
-    data = [float(x) for x in data]
-  sorted_data = sorted(data)
-  length = len(sorted_data)
-  total = sum(sorted_data)
-  average = total / length
-  deviation_list = [value - average for value in sorted_data]
-  deviation_squared_list = [deviation ** 2 for deviation in deviation_list]
-  deviation_total = sum(deviation_squared_list)
-  variance = deviation_total / length
-  standard_deviation = math.sqrt(variance)
-  result = {
-    'sorted_data': sorted_data,
-    'total': total,
-    'len': length,
-    'average': average,
-    'variance': variance,
-    'deviation_list': deviation_list,
-    'standard_deviation': standard_deviation,
-    'deviation_squared_list': deviation_squared_list
-  }
-  return result
-
-def merge(x,y):
-  difference1,Quartile1 = Difference(x),statistics(x)
-  difference2,Quartile2 = Difference(y),statistics(y)
-  result = {
-    'data[1]': {
-      'Quartile': Quartile1,
-      'deviation': difference1,
-    },
-    'data[2]': {
-      'Quartile': Quartile2,
-      'deviation': difference2,
+  def calculate_deviation(self):
+    if isinstance(self.data, str):
+      self.data = self.data.strip('[]').split(',')
+      self.data = [float(x) for x in self.data]
+    sorted_data = sorted(self.data)
+    length = len(sorted_data)
+    total = sum(sorted_data)
+    average = total / length
+    deviation_list = [value - average for value in sorted_data]
+    deviation_squared_list = [deviation ** 2 for deviation in deviation_list]
+    deviation_total = sum(deviation_squared_list)
+    variance = deviation_total / length
+    standard_deviation = math.sqrt(variance)
+    result = {
+      'sorted_data': sorted_data,
+      'total': total,
+      'length': length,
+      'average': average,
+      'variance': variance,
+      'deviation_list': deviation_list,
+      'standard_deviation': standard_deviation,
+      'deviation_squared_list': deviation_squared_list
     }
-  }
-  return result
+    return result
 
-def prime_numbers(start, end):
-  numbers = range(start, end + 1)
-  def is_prime(num):
-    if num <= 1: return False
-    for x in range(2, int(math.sqrt(num)) + 1):
-      if (num % x) == 0: return False
-    return True
-  primes = list(filter(is_prime, numbers))
-  result = {
-    'primes': primes,
-    'start_num': start,
-    'end_num': end,
-  }
-  return result
+  @staticmethod
+  def find_primes(start, end):
+    primes = []
+    for num in range(start, end + 1):
+      if num <= 1:
+        continue
+      is_prime = True
+      for x in range(2, int(math.sqrt(num)) + 1):
+        if (num % x) == 0:
+          is_prime = False
+          break
+      if is_prime:
+        primes.append(num)
+    result = {
+      'primes': primes,
+      'start_num': start,
+      'end_num': end,
+    }
+    return result
+
+  def compare_data(self, data1, data2):
+    self.set_data(data1)
+    quartile1 = self.calculate_statistics()
+    deviation_result1 = self.calculate_deviation()
+    self.set_data(data2)
+    quartile2 = self.calculate_statistics()
+    deviation_result2 = self.calculate_deviation()
+    result = {
+      'data1': {
+        'statistics': quartile1,
+        'deviation': deviation_result1,
+      },
+      'data2': {
+        'statistics': quartile2,
+        'deviation': deviation_result2,
+      }
+    }
+    return result
